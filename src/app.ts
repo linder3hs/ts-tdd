c47c9335727a62fd5f4b32bc312e1b2c4ac1688f
linder3hs <linderhassinger00@gmail.com>
2023-01-31 18:24:38 -0500

    chore: add corse to request

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import { config } from "dotenv";
 import express, { type Application } from "express";
+import cors from "cors";
 
 config();
 
 const app: Application = express();
+
+app.use(cors());
 
 export default app;
