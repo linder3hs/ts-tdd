423098db5366613f0a3da615631ac1f20ad2d808
linder3hs <linderhassinger00@gmail.com>
2023-01-31 16:32:15 -0500

    feat: add jest

--- dist/test/app.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const vitest_1 = require("vitest");
-global.fetch = vitest_1.vi.fn();
-(0, vitest_1.describe)("Probando app", () => {
-    (0, vitest_1.it)("Esperamos que el estado de la app sea 200", () => {
-        (0, vitest_1.expect)(fetch).toHaveBeenCalledWith("http://localhost:9000");
-    });
-});
--- test/app.test.ts
+++ test/app.test.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it } from "@jest/globals";
 import http from "node:http";
 
 describe("Probando app", () => {
