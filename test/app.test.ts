0b2d5c79f65e34beb60eec949e49feecef13df16
Linder Hassinger <linderhassinger00@gmail.com>
2023-02-03 19:21:36 -0500

    Update app.test.ts

--- test/app.test.ts
+++ test/app.test.ts
@@ -3,6 +3,6 @@ import { sumar } from "../src/utils/helper";
 
 describe("primer test", () => {
   it("sumando numeros", () => {
-    expect(sumar(1, 2)).toBe(3);
+    expect(sumar(1, 2)).toBe(4);
   });
 });
